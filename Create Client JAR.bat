c102750c0d6d7b3c6e00386655c0d847addf9cf0
ipavl <ipavl@users.sourceforge.net>
2013-08-29 21:13:33 -0400

    Fixed jar creation scripts, again. I think.

--- Create Client JAR.bat
+++ Create Client JAR.bat
@@ -1,4 +1,5 @@
 @echo off
-jar cfm bin\ClientGUI.jar ClientManifest.txt bin\org\pavlinic\chat\*.class bin\org\pavlinic\chat\client\*.class
+cd bin
+jar cfm ClientGUI.jar ..\ClientManifest.txt org\pavlinic\chat\*.class org\pavlinic\chat\client\*.class
 echo JAR created in bin directory
 pause
--- Create Server JAR.bat
+++ Create Server JAR.bat
@@ -1,4 +1,5 @@
 @echo off
-jar cfm bin\ServerGUI.jar ServerManifest.txt bin\org\pavlinic\chat\*.class bin\org\pavlinic\chat\server\*.class
+cd bin
+jar cfm ServerGUI.jar ..\ServerManifest.txt org\pavlinic\chat\*.class org\pavlinic\chat\server\*.class
 echo JAR created in bin directory
 pause
